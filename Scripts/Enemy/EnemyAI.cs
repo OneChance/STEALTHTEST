49fb8d5ecd5dc46b5b6c5630ae056b68dd274663
1 <1>
2014-11-20 17:00:43 +0800

    1

---
+++ Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+	pubilc float patrolSpeed = 2f;
+	public float chaseSpeed = 5f;
+	public float chaseWaitTime = 5f;
+	public float patrolWaitTime = 1f;
+	public Transform[] patrolWayPoints;
+
+	private EnemySight enemySight;
+	private NavMeshAgent nav;
+	private Transform player;
+	private PlayerHealth playerHealth;
+	private LastPlayerSighting lastPlayerSighting;
+	private float chaseTimer;
+	private float patrolTimer;
+	private wayPointIndex;
+
+	void Awake(){
+		enemySight = GetComponent<EnemySight>();
+		nav = GetComponent<NavMeshAgent>();
+		player = GameObject.FindGameObjectWithTag(Tags.player).transform;
+		playerHealth = player.GetComponent<PlayerHealth>();
+		lastPlayerSighting = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<LastPlayerSighting>();
+		chaseTimer = 0f;
+		patrolTimer = 0f;
+	}
+
+	void Update(){
+		if(enemySight.playerInSight && playerHealth.health>0f){
+			Shooting();
+		}else if(enemySight.personalLastSighting != lastPlayerSighting.resetPosition && playerHealth.health>0f){
+			Chasing();
+		}else{
+			Patrolling();
+		}
+	}
+
+	void Shooting(){
+		nav.Stop();
+	}
+
+	void Chasing(){
+		Vector3 sightingDeltaPos = enemySight.personalLastSighting - transform.position;
+		if(sightingDeltaPos.magnitude>2){
+			nav.destination = enemySight.personalLastSighting;
+		}
+
+		nav.speed = chaseSpeed;
+
+		if(nav.remainingDistance < nav.stoppingDistance){
+
+		}
+	}
+
+	void Patrolling(){
+
+	}
+}
---
+++ Scripts/Enemy/EnemyShooting.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShooting : MonoBehaviour
+{
+		public float maxDamage = 120f;
+		public float minDamage = 45f;
+		public AudioClip shotClip;
+		public float flashIntensity = 3f;
+		public float fadeSpeed = 10f;
+		private Animator anim;
+		private HashIDs hash;
+		private LineRenderer laserShotLine;
+		private Light laserShotlight;
+		private SphereCollider col;
+		private Transform player;
+		private PlayerHealth playerHealth;
+		private bool shooting;
+		private float scaledDamage;
+
+		void Awake ()
+		{
+				anim = GetComponent<Animator> ();
+				hash = GameObject.FindGameObjectWithTag (Tags.gameController).GetComponent<HashIDs> ();
+				laserShotLine = GetComponentInChildren<LineRenderer> ();
+				laserShotlight = laserShotLine.gameObject.light;
+				col = GetComponent<SphereCollider> ();
+				player = GameObject.FindGameObjectWithTag (Tags.player).transform;
+				playerHealth = player.gameObject.GetComponent<PlayerHealth> ();
+
+				laserShotLine.enabled = false;
+				laserShotlight.intensity = 0f;
+
+				scaledDamage = maxDamage - minDamage;
+		}
+
+		void Update ()
+		{
+				float shot = anim.GetFloat (hash.shotFloat);
+				if (shot > 0.5f && !shooting) {
+						Shoot ();
+				}
+				if (shot < 0.5f) {
+						shooting = false;
+						laserShotLine.enabled = false;
+				}
+
+				laserShotlight.intensity = Mathf.Lerp (laserShotlight.intensity, 0f, fadeSpeed * Time.deltaTime);
+		}
+		
+		void OnAnimatorIK (int layerIndex)
+		{
+				float aimWeight = anim.GetFloat (hash.aimWeightFloat);
+				anim.SetIKPosition (AvatarIKGoal.RightHand, player.position + Vector3.up * 1.5f);
+				anim.SetIKPositionWeight (AvatarIKGoal.RightHand, aimWeight);
+		}
+
+		void Shoot ()
+		{
+				shooting = true;
+
+				float damageRatio = (col.radius - Vector3.Distance (player.position, transform.position)) / col.radius;
+				float damage = minDamage + scaledDamage * damageRatio;
+				playerHealth.TakeDamage (damage);
+				ShotEffects ();
+				
+		}
+
+		void ShotEffects ()
+		{
+				laserShotLine.SetPosition (0, laserShotlight.transform.position);
+				laserShotLine.SetPosition (1, player.position + Vector3.up * 1.5f);
+				laserShotLine.enabled = true;
+				laserShotlight.intensity = flashIntensity;
+				AudioSource.PlayClipAtPoint (shotClip, laserShotlight.transform.position);
+		}
+}
